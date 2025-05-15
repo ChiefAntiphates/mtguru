dbbb5985a779e72fb7c01f6ab9c2b9720768f04c
Abdul-213 <m.abdulhannan.ahmad@gmail.com>
2025-05-15 22:49:41 +0100

    fixing stuff for npm run build

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import SearchBar from './components/SearchBar'
 import Filters from './components/Filters'
 import CardGrid from './components/CardGrid'
