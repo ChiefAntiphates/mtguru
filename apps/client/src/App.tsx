4ea408930c67716cdbefcc5b89df550cd4543dc3
Abdul-213 <m.abdulhannan.ahmad@gmail.com>
2025-05-19 20:13:26 +0100

    fix CORS error

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -56,7 +56,9 @@ function App() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Origin': 'https://mtguru.com',
+          'Access-Control-Allow-Methods': 'GET, POST',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         },
         body: JSON.stringify({
           "query": query,
